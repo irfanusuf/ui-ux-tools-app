76dda8b26f6a1fe005b242d4d2aeff0b4dfacba4
irfanusuf <irfanusuf>
2023-06-07 08:00:48 +0000

    Pushed from Locofy Builder

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import {
+  Routes,
+  Route,
+  useNavigationType,
+  useLocation,
+} from "react-router-dom";
+import Desktop1 from "./pages/Desktop1";
+import Desktop2 from "./pages/Desktop2";
+import { useEffect } from "react";
+
+function App() {
+  const action = useNavigationType();
+  const location = useLocation();
+  const pathname = location.pathname;
+
+  useEffect(() => {
+    if (action !== "POP") {
+      window.scrollTo(0, 0);
+    }
+  }, [action, pathname]);
+
+  useEffect(() => {
+    let title = "";
+    let metaDescription = "";
+
+    switch (pathname) {
+      case "/":
+        title = "";
+        metaDescription = "";
+        break;
+      case "/desktop-2":
+        title = "";
+        metaDescription = "";
+        break;
+    }
+
+    if (title) {
+      document.title = title;
+    }
+
+    if (metaDescription) {
+      const metaDescriptionTag = document.querySelector(
+        'head > meta[name="description"]'
+      );
+      if (metaDescriptionTag) {
+        metaDescriptionTag.content = metaDescription;
+      }
+    }
+  }, [pathname]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Desktop1 />} />
+      <Route path="/desktop-2" element={<Desktop2 />} />
+    </Routes>
+  );
+}
+export default App;
